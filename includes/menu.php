1a08d00decbc8bd6b8c1f7ec0f38f64d4cd71476
Rellyx <pa_di_santi@hotmail.com>
2013-11-08 11:39:03 -0200

    Menu remodelado. 	Função de "fade", aba com imagem (home), menu subdividido em cores.

--- includes/menu.php
+++ includes/menu.php
@@ -1,76 +1,25 @@
+
 <html>
     <head>
+        <meta charset="utf-8" />
+
         <title>Menu</title>
-            
-        <style type="text/css">
-            ul {
-                float: left;
-                list-style-type: none;
-                margin: 1px;
-                padding: 1px;
-                padding-top:7px;
-                padding-bottom:5px;
-            }
 
-            li {
-                float:left;
-                position:relative; 
-                padding-right: auto; 
-                display:inline;
-            }
+        <link rel="stylesheet" href="css/styles.css" />
 
-            a:link,a:visited {
-                font-weight:bold;
-                text-align:center;
-                padding:6px;
-                text-decoration: aliceblue;
-                text-transform:uppercase;
-            }
-
-            a:hover,a:active *{
-                background-color: azure;
-            }
-            
-            #horizontalmenu li ul {
-                display:none;
-                position:absolute;
-            }
-
-            #horizontalmenu li:hover ul{
-                display:block;
-                background:azure;
-                height:auto; 
-                width:8em;
-            }
-            
-            #horizontalmenu li ul li{
-                clear:both;
-                border-style:none;
-            }
-
-        </style>
     </head>
-    
-    <body>
-    
-        <div id="horizontalmenu"> 
-        
-            <ul> <li> <a href="#"> Home </a> </li> </ul>
 
-            <ul> <li><a href="#Quem somos">Quem somos</a>
-                <ul> <li> <a href="#">Unifal</a></li> <li><a href="#">Tribit Jr.</a></li></ul> </li>
-                
-            <li> <a href="#CONTATO">Contato</a>
-                <ul> <li> <a href="#">Endereço</a></li> <li><a href="#">Telefone</a></li> </ul> </li>
-            
-            <li> <a href="#MAPAS">Mapas</a>
-                <ul> <li> <a href="#">Casas</a></li> <li><a href="#">Prédios</a></li> </ul> </li>
-            
-            <li> <a href="#ADMIN">Admin</a>
-                <ul> <li> <a href="#">Login</a></li>
-                
-        </div>
-        
+    <body>
+
+        <nav>
+            <ul class="fancyNav">
+                <li id="home"><a href="#home" class="homeIcon">Home</a></li>
+                <li id="news"><a href="#news">Mapas</a></li>
+                <li id="about"><a href="#about">Sobre nós</a></li>
+                <li id="services"><a href="#services">Services</a></li>
+                <li id="contact"><a href="#contact">Contato</a></li>
+            </ul>
+        </nav>
+
     </body>
-    
 </html>
